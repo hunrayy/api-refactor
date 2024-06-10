98faa846745057ce2d621800e75e50c1578f45bd
hunrayy <henryokiyi8@gmail.com>
2024-06-10 16:56:28 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,10 @@
+import requests
+datas = requests.get("https://www.jumia.com.ng/")
+from bs4 import BeautifulSoup
+# print(datas.content)
+feed = datas.content
+feed_content = BeautifulSoup(feed, "html.parser")
+feed_prett = feed_content.prettify()
+contents = feed_content.findAll("img")
+for img in contents:
+    print("img")
