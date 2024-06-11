e2caf9031b8cb3f2ae2a2a37e76fac566611c616
hunrayy <henryokiyi8@gmail.com>
2024-06-11 13:11:15 +0100

    scrapped images from a site

--- index.py
+++ index.py
@@ -1,8 +1,21 @@
 import requests
 datas = requests.get("https://www.jumia.com.ng/")
 from bs4 import BeautifulSoup
-# print(datas.content)
 feed = datas.content
 feed_content = BeautifulSoup(feed, "html.parser")
 feed_prett = feed_content.prettify()
-contents = feed_content.findAll("img")
+images = feed_content.findAll("img")
+image_array = []
+for image in images:
+    data_src = image.get("data-src")
+    if(data_src):
+        image_array.append(data_src)
+        # print(image_array)
+
+print(len(image_array))
+
+
+# print(contents)
+
+
+# print("hello")
